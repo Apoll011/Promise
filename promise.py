6d66ee6783deef74e881acfe248e4817b178edd1
Apoll0_11 <80171544+Apoll011@users.noreply.github.com>
2024-06-14 22:51:19 -0100

    Create promise.py
    Added the promise file

---
+++ promise.py
@@ -0,0 +1,48 @@
+import threading
+
+class Promise:
+    def __init__(self):
+        self.resolved = False
+        self.rejected = False
+        self.value = None
+        self.error = None
+        self.then_callback = None
+        self.catch_callback = None
+        self.lock = threading.Lock()  # Lock to ensure thread-safe access
+
+    def then(self, callback):
+        with self.lock:
+            if self.resolved:
+                callback(self.value)
+            else:
+                self.then_callback = callback
+        return self
+
+    def catch(self, callback):
+        with self.lock:
+            if self.rejected:
+                callback(self.error)
+            else:
+                self.catch_callback = callback
+        return self
+
+    def resolve(self, value):
+        threading.Thread(target=self.do_resolve, args=(value,)).start()
+
+    def do_resolve(self, value):
+        with self.lock:
+            if not self.resolved and not self.rejected:
+                v = value()
+                if v:
+                    self.value = v
+                    self.resolved = True
+                    if self.then_callback:
+                        self.then_callback(v)
+
+    def reject(self, error):
+        with self.lock:
+            if not self.rejected and not self.resolved:
+                self.rejected = True
+                self.error = error
+                if self.catch_callback:
+                    self.catch_callback(error)
